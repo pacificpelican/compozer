e5a19aa57c47d6502b065d086595fce54143bba2
Daniel McKeown <dj@sf3am.com>
2023-10-03 21:37:40 -0700

    update with home page and Post

---
+++ web/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,19 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+      <h1>Compoze[r]</h1>
+      <h3>
+        `<Link to={routes.home()}>Home</Link>`
+      </h3>
+      <p>
+        <code>TEXT EDITOR INTERFACE ARCHITECTURAL TOOL</code>
+      </p>
+    </>
+  )
+}
+
+export default HomePage
