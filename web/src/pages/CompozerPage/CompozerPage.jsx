3f994e001585ae501078be1a22b7b5984a642696
Daniel McKeown <2019@sf3am.com>
2023-10-17 17:43:08 -0700

    add local postform

--- web/src/pages/CompozerPage/CompozerPage.jsx
+++ web/src/pages/CompozerPage/CompozerPage.jsx
@@ -1,6 +1,6 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-import NewPost from 'src/components/Post/NewPost'
+import NewPost from 'src/components/Compozer/NewPost'
 
 const CompozerPage = () => {
   return (
